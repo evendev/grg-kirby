a149e7043684b467d2c66e2586ce3830bf5ea3ae
stevejamesson <steve@evendev.com>
2015-12-28 08:10:41 -0500

    tripadvisor

--- site/templates/places/socials.blade.php
+++ site/templates/places/socials.blade.php
@@ -3,6 +3,11 @@
         <i class="fa fa-facebook-square icon-lg"></i>
     </a>
 @endif
+@if ($item->tripadvisorUrl()->isNotEmpty())
+    <a href="{{ $item->tripadvisorUrl() }}" target="_blank">
+        <i class="fa fa-tripadvisor icon-lg"></i>
+    </a>
+@endif
 @if ($item->twitterUrl()->isNotEmpty())
     <a href="{{ $item->twitterUrl() }}" target="_blank">
         <i class="fa fa-twitter-square icon-lg"></i>
