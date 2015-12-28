9795d35331e42c11b29cdc84ab6d77302ba58428
stevejamesson <steve@evendev.com>
2015-12-28 08:13:44 -0500

    tweak tripadvisor/icon styles

--- site/templates/places/socials.blade.php
+++ site/templates/places/socials.blade.php
@@ -1,20 +1,20 @@
-@if ($item->facebookUrl()->isNotEmpty())
-    <a href="{{ $item->facebookUrl() }}" target="_blank">
-        <i class="fa fa-facebook-square icon-lg"></i>
-    </a>
-@endif
 @if ($item->tripadvisorUrl()->isNotEmpty())
     <a href="{{ $item->tripadvisorUrl() }}" target="_blank">
-        <i class="fa fa-tripadvisor icon-lg"></i>
+        <i class="fa-social fa fa-tripadvisor icon-lg"></i>
+    </a>
+@endif
+@if ($item->facebookUrl()->isNotEmpty())
+    <a href="{{ $item->facebookUrl() }}" target="_blank">
+        <i class="fa-social fa fa-facebook-square icon-lg"></i>
     </a>
 @endif
 @if ($item->twitterUrl()->isNotEmpty())
     <a href="{{ $item->twitterUrl() }}" target="_blank">
-        <i class="fa fa-twitter-square icon-lg"></i>
+        <i class="fa-social fa fa-twitter-square icon-lg"></i>
     </a>
 @endif
 @if ($item->instagramUrl()->isNotEmpty())
     <a href="{{ $item->instagramUrl() }}" target="_blank">
-        <i class="fa fa-instagram icon-lg"></i>
+        <i class="fa-social fa fa-instagram icon-lg"></i>
     </a>
 @endif
