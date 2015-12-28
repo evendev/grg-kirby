71810045eb7e5822a7a84c0690ea658f9e1c5270
stevejamesson <steve@evendev.com>
2015-12-28 08:14:35 -0500

    add titles to social icons

--- site/templates/places/socials.blade.php
+++ site/templates/places/socials.blade.php
@@ -1,20 +1,20 @@
 @if ($item->tripadvisorUrl()->isNotEmpty())
-    <a href="{{ $item->tripadvisorUrl() }}" target="_blank">
+    <a href="{{ $item->tripadvisorUrl() }}" target="_blank" title="{{ $item->title() }} on TripAdvisor">
         <i class="fa-social fa fa-tripadvisor icon-lg"></i>
     </a>
 @endif
 @if ($item->facebookUrl()->isNotEmpty())
-    <a href="{{ $item->facebookUrl() }}" target="_blank">
+    <a href="{{ $item->facebookUrl() }}" target="_blank" title="{{ $item->title() }} on Facebook">
         <i class="fa-social fa fa-facebook-square icon-lg"></i>
     </a>
 @endif
 @if ($item->twitterUrl()->isNotEmpty())
-    <a href="{{ $item->twitterUrl() }}" target="_blank">
+    <a href="{{ $item->twitterUrl() }}" target="_blank" title="{{ $item->title() }} on Twitter">
         <i class="fa-social fa fa-twitter-square icon-lg"></i>
     </a>
 @endif
 @if ($item->instagramUrl()->isNotEmpty())
-    <a href="{{ $item->instagramUrl() }}" target="_blank">
+    <a href="{{ $item->instagramUrl() }}" target="_blank" title="{{ $item->title() }} on Instagram">
         <i class="fa-social fa fa-instagram icon-lg"></i>
     </a>
 @endif
