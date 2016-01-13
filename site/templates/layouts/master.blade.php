602eb70b6759849e1fc61b8ab4958bc58505d0ce
stevejamesson <steve@evendev.com>
2016-01-13 12:02:45 -0500

    add ie fallback scripts

--- site/templates/layouts/master.blade.php
+++ site/templates/layouts/master.blade.php
@@ -11,6 +11,11 @@
         <link rel="stylesheet" href="/assets/css/app.css">
 
         @yield('head')
+
+        <!--[if lt IE 9]>
+          <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
     <body>
         <div class="site">
